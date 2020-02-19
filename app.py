45deecf8bb3408a9cab2045748ca2a2887918b05
rittik97 <rg2870@columbia.edu>
2020-02-18 21:35:22 -0500

    the 6 to demo

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import os
 #os.chdir('/Users/rittikghosh/Documents/Python/climate/')
 from werkzeug import secure_filename
 import cv2 as cv2
-#from PIL import Image
+from PIL import Image
 from tensorflow.keras.models import load_model
 
 app = flask.Flask(__name__, template_folder='templates')
@@ -25,21 +25,43 @@ def index():
     with open("templates/home.html", 'r') as p:
        return p.read()
 
+@app.route('/classify', methods=['POST'])
+def trial():
+        args= (flask.request.form)
+        url=args['data']
+        img_name=url.split('images/')[1]
+        #img_name='PermanentCrop_514.jpg'
+        path='static/images/'+img_name
+        img = Image.open(path)
+        img=np.asarray(img, dtype='float32')
+        img=img.reshape(1,64,64,3)
+        model=load_model('model_best_2.h5')
+        pred=model.predict_classes(img)[0]
+        print(pred)
+        if pred==4:
+                return 'Predicted: Built-up Land'
+        return 'Predicted: Vegetation'
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
    if flask.request.method == 'POST':
       f = flask.request.files['file']
-      img=(np.fromstring(f.read(), np.uint8))
+
+      #img=(np.fromstring(f.read(), dtype='int32'))
+      #f=flask.request
+      img = np.fromstring(f.read(), np.uint8)
+
       img = cv2.imdecode(img, cv2.IMREAD_UNCHANGED)
+      img = Image.fromarray(img.astype("uint8"))
+      print(img)
       f.save(os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(f.filename)))
-      img = cv2.resize(img, (64, 64), interpolation = cv2.INTER_AREA)
+      #img = cv2.resize(img, (64, 64), interpolation = cv2.INTER_AREA)
       #cv2.imwrite('/Users/rittikghosh/Documents/Python/climate/test.png',img)
-      #img=cv2.imread('test.png', cv2.IMREAD_UNCHANGED)
-      #img.shape
+      #img=cv2.imread('Forest_338.jpg', cv2.IMREAD_UNCHANGED)
+      img=np.asarray(img, dtype='float32')
       model=load_model('model-best.h5')
       img=img.reshape(1, 64,64,3)
 
-      return str(model.predict_on_batch(img))
+      return str(model.predict_classes(img))
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -12,3 +12,31 @@ if (input.files && input.files[0]) {
     reader.readAsDataURL(input.files[0]);
 }
 }
+
+function classify(id){
+  x=document.getElementById(id)
+  SendAjax(x.querySelector(".card-img-top").src,x);
+}
+function SendAjax(url,elem){
+
+
+    $.post('/classify',
+        { data: url},
+        function(data, status, xhr) {
+          x.querySelector(".card-text").innerHTML=data
+          
+          console.log(data2)
+          console.log(data2/(data['bill']))
+            if (data2/(data['bill'])<=0.14){
+              temp=Math.round(data['bill']*0.14)
+              document.getElementById("tip").innerHTML='Suggested total tip amount: '+temp;
+            }else{
+              document.getElementById("tip").innerHTML='Suggested total tip amount: '+data2;
+            }
+            form.reset()
+
+            //alert('status: ' + status + ', data: ' + data);
+
+        }).done(function() {  })
+        .fail(function(jqxhr, settings, ex) { alert('failed, ' + ex); });
+}
